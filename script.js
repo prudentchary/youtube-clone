c43dfa1bb6d4e67ea59c3fd514a70200cebcf5f6
Prudent chary <akannibenjamin200@gmail.com>
2024-07-09 14:50:45 +0600

    first commit

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
+var header = document.getElementById("header");
+var searchButton = document.getElementsById("searchbtn");
 
 function handleClick() {
-    var header = document.getElementById("header");
-    var searchButton = document.getElementsById("searchbtn");
-
+   
     alert('Button was clicked');
 }
 
 searchButton.addEventListener('click', handleClick);
-console.log('ima biy')
